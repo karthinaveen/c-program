876f87a565425b92e42219eaf1dfa33dce458565
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-02-07 14:11:49 +0530

    Create e.c

---
+++ e.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int hrs1,hrs2,mins1,mins2,a,b;
+printf("Enter the hrs1 and mins1:");
+scanf("%d%d",&hrs,&mins);
+printf("Enter the hrs2 and mins2:");
+scanf("%d%d",&hrs,&mins);
+a=hrs1-hrs2;
+b=mins1-mins2;
+printf("%d %d",a,b);
+return 0;
+ }

e7522565aed21d9e7e14f3c8c45c54f8a9c496d8
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-02-07 15:12:01 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d\n",&n);
+	switch(n)
+	{
+		case 1:
+		printf("one");
+		break;
+		case 2:
+		printf("two");
+		break;
+		case 3:
+		printf("three");
+		break;
+		case 4:
+		printf("four");
+		break;
+		case 5:
+		printf("five");
+		break;
+		default:
+		printf(" invalid number");
+	 }
+	return 0;
+                }

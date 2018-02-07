2de3f54946cf16f708d49cff5ec3a2ca595a5d42
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-02-07 14:03:52 +0530

    Create o.c

---
+++ o.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <time.h>
+std::string gen_random() {
+size_t len = rand() % 25 + 5;
+
+    std::string x;
+}
+
+static const int num = 10000000;
+
+int main(){
+    std::vector<std::string> strings;
+    clock_t start = clock();
+    std::sort(strings.begin(), strings.end());
+    clock_t ticks = clock() - start;
+
+
+    return 0;
+}

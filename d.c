ca1ab2fcea2814cdc70af1214347e7da808eb356
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-02-07 14:09:43 +0530

    Create d.c

---
+++ d.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int mins,hrs,rmin;
+printf("Enter the minutes:");
+scanf("%d",&mins);
+hrs=mins/60;
+rmin=mins%60;
+printf("%d %d",hrs,rmin);
+}
+
+© 2018 GitHub, Inc.

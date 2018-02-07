821ccdf9fa228bd7c2061a8f8a125919a4a4829e
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-02-07 14:49:23 +0530

    Create u.c

---
+++ u.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	 char c[20],d[20];
+	int a,b;
+	printf("\n enter the two strings");
+	scanf("%s %s",&a,&b);
+	a=strlen(c);
+	b=strlen(d);
+	if(c>=d)
+	{
+		printf("\n %s",c);
+	}
+	else
+	{
+	printf("\n %s",d);
+	}
+}

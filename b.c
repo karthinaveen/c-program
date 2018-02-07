877c55c03ed39d5beca8257a733853b0217a4ad9
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-02-07 14:07:52 +0530

    Create b.c

---
+++ b.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,n,a[7];
+printf("enter the limits");
+scanf("%d",&n);
+print("enter the elements");
+for(i=0;i<n;i++)
+ {
+scanf("%d",&a[i]);
+printf("%d %d",a[i],i);
+}
+}

ca35379229eb48685de66285b446c34c7de1cede
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-02-07 15:06:02 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void  main()
+{
+int a, b, c;
+printf("Enter two numbers to add\n");
+scanf("%d%d", &a, &b);
+c = a + b;
+printf("Sum of the numbers = %d\n", c);
+getch();
+}

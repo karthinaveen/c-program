c242156ad25b7a1949d09acc46dd73e3951793ed
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-02-28 14:04:32 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   int x=0,y=1,z,i,n;
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       printf("%d ",x);
+       z=x+y;
+      x=y;
+      y=z;
+   }
+   
+    return 0;
+}
